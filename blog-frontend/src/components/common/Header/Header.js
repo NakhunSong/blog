e6a9cb8dbc0cc9aa820f868dee75ced8bde42198
NakhunSong <nak4627@gmail.com>
2019-02-15 22:41:33 +0900

    Add Button component

--- blog-frontend/src/components/common/Header/Header.js
+++ blog-frontend/src/components/common/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Header.scss';
 import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
+import Button from 'components/common/Button';
 
 const cx = classNames.bind(styles);
 
@@ -10,11 +11,10 @@ const Header = () => {
         <header className={cx('header')}>
             <div className={cx('header-content')}>
                 <div className={cx('brand')}>
-                    <Link to="/">reactblog</Link>
+                    <Link to="/">Khun's Blog</Link>
                 </div>
                 <div className={cx('right')}>
-                    { }
-                    오른쪽
+                    <Button theme="outline" to="/editor">새 포스트</Button>
                 </div>
             </div>
         </header>
