a2dce4a24b9dfdc02feb968c4c5b590ac207c0e0
NakhunSong <nak4627@gmail.com>
2019-02-18 18:28:47 +0900

    Implement editor status management by adding editor container and store

--- blog-frontend/src/pages/EditorPage.js
+++ blog-frontend/src/pages/EditorPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import EditorHeader from 'components/editor/EditorHeader';
 import EditorTemplate from 'components/editor/EditorTemplate';
-import EditorPane from 'components/editor/EditorPane';
+import EditorPaneContainer from 'containers/editor/EditorPaneContainer';
 import PreviewPane from 'components/editor/PreviewPane';
 
 const EditorPage = () => {
@@ -9,7 +9,7 @@ const EditorPage = () => {
         <div>
             <EditorTemplate
                 header={<EditorHeader/>}
-                editor={<EditorPane/>}
+                editor={<EditorPaneContainer/>}
                 preview={<PreviewPane/>}
             >
             </EditorTemplate>
