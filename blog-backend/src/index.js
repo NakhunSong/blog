90252907e1016e9ab1d7caae24195328607e8e36
NakhunSong <nak4627@gmail.com>
2019-03-05 23:53:10 +0900

    Fix index.js on backend

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -6,6 +6,8 @@ const bodyParser = require('koa-bodyparser');
 
 const api = require('./api');
 
+const ssr = require('./ssr');
+
 const mongoose = require('mongoose');
 
 const session = require('koa-session');
@@ -31,6 +33,7 @@ const router = new Router();
 
 // 라우터 설정
 router.use('/api', api.routes());
+router.get('/', ssr);
 
 // 라우터 적용 전에 bodyParser 적용
 app.use(bodyParser());
@@ -46,6 +49,8 @@ app.keys = [signKey];
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
+
+app.use(ssr); // 일치하는 것이 없으면 ssr을 실행합니다.
 
 app.listen(port, () => {
     console.log('listening to port ', port);
